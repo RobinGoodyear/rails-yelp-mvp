aca9e3b692b19a9db2c906f018a11152d121b67b
Robin Goodyear <robingoodyear6@gmail.com>
2023-02-22 16:21:31 +0100

    not finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    resources :restaurants, only: %i[create index new show]
+    resources :review, only: %i[create new]
+  end
 end
